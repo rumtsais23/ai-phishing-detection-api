585d0e7f04e07b72866022f77957ec72e935e9b9
rumsatsais23 <rumen.petkov85@gmail.com>
2026-04-22 22:50:27 +0300

    fix missing security scan endpoint

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from routes.phishing import phishing_bp
+from routes.security import security_bp
+from routes.analyze import analyze_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(phishing_bp)
+app.register_blueprint(security_bp)
+app.register_blueprint(analyze_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
